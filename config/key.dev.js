7a5b9c2919ea92efa9fe442da6906652625a7e90
Sergey Kardash <kardashsergeydev@gmail.com>
2019-04-18 13:38:59 +0300

    asdf

--- config/key.dev.js
+++ config/key.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://Greg:comcodes_2019@cluster0-gbz7w.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://Greg:comcodes_2019@cluster0-gbz7w.mongodb.net/Comcodes?retryWrites=true',
   jwt: 'comcodes-jwt',
   fcmApiKey: 'AAAAQMjlAVk:APA91bGfCLkjG6n8PWxLnvLJ8mF3Mw5_vTd7vStZFGHTHbePA8p4vjbxoLnGcGdk3zb70NnY20lnVmTeoqSvzuJ5PM-jn2RF1PTh8vBJGpCHMwD9TcgIn-IUFY07yYteTnf0kGZDFMk-'
 }
