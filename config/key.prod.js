4d60d9e6e91a4ec7b46c124ea14d5016ea6cb833
Sergey Kardash <kardashsergeydev@gmail.com>
2019-07-28 11:35:24 +0300

    change spy prod key

--- config/key.prod.js
+++ config/key.prod.js
@@ -1,6 +1,7 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   jwt: process.env.JWT,
-  tetrisFcmApiKey: process.env.TETRIS_FCM_API_KEY,
-  aCleanerFcmApiKey: process.env.ACLEANER_FCM_API_KEY
+  spyFcmApiKey: process.env.SPY_FCM_API_KEY
+  // tetrisFcmApiKey: process.env.TETRIS_FCM_API_KEY,
+  // aCleanerFcmApiKey: process.env.ACLEANER_FCM_API_KEY
 }
